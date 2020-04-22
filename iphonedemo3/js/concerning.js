21b81b6d0d8c80625eb03730c547939383b0996d
chenxiaofeng <chenxiaofeng.988@163.com>
2020-04-23 07:27:09 +0800

    ss

--- iphonedemo3/js/concerning.js
+++ iphonedemo3/js/concerning.js
@@ -4,20 +4,14 @@ pShow(1,64)
 pShow(2,44)
 function pShow(id,num){
 	content.push($("#concer_main1_p"+id).html())
-	// console.log(content[id-1])
 	test2.push($("#concer_main1_p"+id).html().slice(0,num))
-	// console.log(test2[id-1])
 	$("#concer_main1_p"+id).html(test2[id-1])
-	// console.log(test2);
 	var span = document.createElement('span');
 	span.innerHTML = '...'
 	 $("#concer_main1_p"+id).append(span)
-	
 }
 function aboutBtn(id){
 	var thisBtn=$(".main_inner").eq(id).find(".about_btn_1")
-	
-	console.log(thisBtn.text())
 	if(thisBtn.text()=="了解更多>>"){
 		thisBtn.text("收起>>")
 		thisBtn.parent().find(".concer_main1_ps").eq(0).html(content[id])
@@ -32,7 +26,6 @@ function aboutBtn(id){
 $(".about_btn_1").on({
 	"click":function(){
 		var _idx=$(this).parent().parent().index()
-		console.log(_idx)
 		aboutBtn(_idx)
 	}
 })
